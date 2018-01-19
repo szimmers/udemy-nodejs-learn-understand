e82fef11edac74800e53cb846673e71ab16f1450
Steve Zimmers <steve@soccer-1.com>
2018-01-19 14:43:16 -0500

    add some require notes

--- s4/app4.js
+++ s4/app4.js
@@ -1,2 +1,9 @@
 var greet = require('./greet');
 greet();
+
+// after required, node will wrap up greet like this:
+/*
+(function(exports, require, module, __filename, __dirname) {
+	// greet
+});
+ */
