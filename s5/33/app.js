bb10b3f6f526abdee91f19b9af146450b338e63d
Steve Zimmers <steve@soccer-1.com>
2018-01-22 14:15:21 -0500

    - create config for event emitter - demo that core events works same way as our demo

--- s5/33/app.js
+++ s5/33/app.js
@@ -1,13 +1,17 @@
 var Emitter = require('./emitter');
+// this works too, interface is the same
+//var Emitter = require('events');
+
+var eventConfig = require('./config').events;
 
 var emitter = new Emitter();
 
-emitter.on('greet', function() {
+emitter.on(eventConfig.GREET, function() {
 	console.log('greeted fired (a)');
 });
 
-emitter.on('greet', function() {
+emitter.on(eventConfig.GREET, function() {
 	console.log('greeted fired (b)');
 });
 
-emitter.emit('greet');
+emitter.emit(eventConfig.GREET);
