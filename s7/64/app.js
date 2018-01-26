780a6c3e95683df507856dc1a53e85a4698f9e0f
Steve Zimmers <steve@soccer-1.com>
2018-01-26 10:18:32 -0500

    routing intro

---
+++ s7/64/app.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer(function(req, res) {
+	if (req.url === '/') {
+		let path = `${__dirname}/index.html`;
+		fs.createReadStream(path).pipe(res);
+	}
+	else if (req.url === '/api') {
+		res.writeHead(200, {'Content-Type': 'application/json'});
+
+		let path = `${__dirname}/index.html`;
+
+		let obj = {
+			brick: 'soup',
+			chair: 'tree'
+		};
+
+		res.end(JSON.stringify(obj));
+	}
+	else {
+		res.writeHead(404);
+		res.end();
+	}
+
+}).listen(1337, '127.0.0.1');
